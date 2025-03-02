03e1333db6a17143372c165c564bbc6b3d9647c1
aache <aleksandra.cheremiskina@gmail.com>
2025-03-02 18:39:38 +0300

    add

--- src/exercise4.kt
+++ src/exercise4.kt
@@ -5,7 +5,7 @@ fun main() {
     var number2 = 0.0
     var sign = ' '
     while (!isValidInput){
-        println("Введите два числа и символ операции (+,-,*,/) в формате ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ:")
+        println("Введите два числа и символ операции (+,-,*,/) через пробел в формате ЧИСЛО1 ЧИСЛО2 ОПЕРАЦИЯ:")
         userInput = readln()
         val words = userInput.split(" ")
         if(words.count() == 3 && words[0].toDoubleOrNull() != null && words[1].toDoubleOrNull() != null && words[2].count() == 1 && words[2].firstOrNull() in setOf('+','-','/','*')){
@@ -14,9 +14,9 @@ fun main() {
             number2 = words[1].toDouble()
             sign = words[2].first()
         }
-
-        else
+        else{
             println("Введено неверное значение. Повторите ввод.")
+        }
     }
     val result = when (sign){
         '+' -> number1+number2
